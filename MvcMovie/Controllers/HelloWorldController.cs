5bdfb38bbd7119f4695da6d7e9d8f091a301e4a4
nataliamissio <nataalia.missio@gmail.com>
2021-06-20 18:27:05 -0300

    Adicionado Controller e View

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
